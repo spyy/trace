afc36db8c2fa003ceabff6425597fc69e708fb55
Sami Pyy <sami.pyy@gmail.com>
2017-11-28 10:56:20 +0000

    ...

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (error, req, res) {
+app.get('/', function (req, res) {
   console.log(req.body);
-  res.send('Hei mualima!');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hei mualima\n');
 });
 
 app.listen(process.env.PORT, process.env.IP, function () {
- 
   console.log('Trace app listening on port: ' + process.env.PORT);
 });
